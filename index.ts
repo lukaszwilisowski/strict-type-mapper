ec06cfe60959145dbe641f98f44455679698bcaf
Lukasz Wilisowski <lukasz.wilisowski@willcodeacademy.com>
2024-02-14 23:29:51 +0100

    index

---
+++ index.ts
@@ -0,0 +1,5 @@
+import { MapTo } from './src/helpers/map.to.helper';
+import { Mapping } from './src/interfaces/mapping.interface';
+import { StrictTypeMapper } from './src/strict.type.mapper';
+
+export { MapTo, StrictTypeMapper, type Mapping };
