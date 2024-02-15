ad124c857bd2018e56b56da45b69a40d7f9fd9ee
Lukasz Wilisowski <lukasz.wilisowski@willcodeacademy.com>
2024-02-15 16:23:59 +0100

    added transform pytes

--- index.ts
+++ index.ts
@@ -1,6 +1,21 @@
 import { MapTo } from './src/helpers/map.to.helper';
 import { Mapping } from './src/interfaces/mapping.interface';
+import {
+  TransformArray,
+  TransformArrayOfObjects,
+  TransformNestedObject,
+  TransformProperty
+} from './src/interfaces/mapping.transforms';
 import { CompiledMapping } from './src/models/compiled.mapping';
 import { StrictTypeMapper } from './src/strict.type.mapper';
 
-export { MapTo, StrictTypeMapper, type CompiledMapping, type Mapping };
+export {
+  MapTo,
+  StrictTypeMapper,
+  type CompiledMapping,
+  type Mapping,
+  type TransformArray,
+  type TransformArrayOfObjects,
+  type TransformNestedObject,
+  type TransformProperty
+};
