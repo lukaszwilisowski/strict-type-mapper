864ea1a3d7fa98939c600c94dcc90452c83024d7
Lukasz Wilisowski <lukasz.wilisowski@willcodeacademy.com>
2024-02-15 14:21:32 +0100

    exported

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import { MapTo } from './src/helpers/map.to.helper';
 import { Mapping } from './src/interfaces/mapping.interface';
+import { CompiledMapping } from './src/models/compiled.mapping';
 import { StrictTypeMapper } from './src/strict.type.mapper';
 
-export { MapTo, StrictTypeMapper, type Mapping };
+export { MapTo, StrictTypeMapper, type CompiledMapping, type Mapping };
